bf11b8009bcdfdb9e5acb2ee41b0d04e5977e5d4
Mohamed Ashraf <mohamed.ashraf.z879@gmail.com>
2021-08-24 00:15:42 +0200

    first prototype of merge sort algorithm

---
+++ practice/Merge_Sort/MergeSortP1.java
@@ -0,0 +1,49 @@
+package merge_sort;
+
+public class MergeSortP1 {
+    private static boolean less(Comparable a, Comparable b) {
+        return (a.compareTo(b) < 0);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i < hi; i++)
+            if (less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+
+    private static void sort(Comparable[] a) {
+        int N = a.length;
+        Comparable[] aux = new Comparable[N];
+
+        for (int sz = 1; sz < N; sz = sz + sz)
+            for (int lo = 0; lo < N - sz; lo += sz + sz)
+                merge(a, aux, lo, lo + sz - 1, Math.min(lo + 2 * sz - 1, N - 1));
+    }
+
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid + 1, hi);
+
+        for (int k = lo; k <= hi; k++)
+            aux[k] = a[k];
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k < hi; k++) {
+            if (i > mid)
+                a[k] = aux[j++];
+            else if (j > hi)
+                a[k] = aux[i++];
+            else if (less(aux[j], aux[i]))
+                a[k] = aux[j++];
+            else
+                a[k] = aux[i++];
+        }
+        assert isSorted(a, lo, hi);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hi there");
+    }
+
+}
