f4e48c350e684306f5b46c9d75b411a70f7422e2
Mohamed Ashraf <mohamed.ashraf.z879@gmail.com>
2021-09-27 02:24:51 +0200

    core implemetation is done for SAP.class and myBFS.class

--- wordnet/MyBFS.java
+++ wordnet/MyBFS.java
@@ -9,7 +9,7 @@ import edu.princeton.cs.algs4.Queue;
 
 import java.util.ArrayList;
 
-public class MyBFS {
+class MyBFS {
     private Digraph G;
     // marked array for each vertex
     private boolean markedS[];
@@ -18,13 +18,29 @@ public class MyBFS {
     private int disToS[];
     private int disToW[];
     // edgeTo array for each vertex
-    private int edgeToS[];
-    private int edgeToW[];
+    // private int edgeToS[];
+    // private int edgeToW[];
+    // ancestor and shortest distance
+    private int ancestor;
+    private int shortestDis;
+    // path queue
+    // private LinkedList<Integer> path;
 
     MyBFS(Digraph G) {
         this.G = G;
         markedS = new boolean[G.V()];
         markedW = new boolean[G.V()];
+        // edgeToS = new int[G.V()];
+        // edgeToW = new int[G.V()];
+        disToS = new int[G.V()];
+        disToW = new int[G.V()];
+        ancestor = -1;
+        shortestDis = Integer.MAX_VALUE;
+
+        for (int v = 0; v < G.V(); v++) {
+            disToS[v] = Integer.MAX_VALUE;
+            disToW[v] = Integer.MAX_VALUE;
+        }
     }
 
     void bfs(int s, int w) {
@@ -37,12 +53,13 @@ public class MyBFS {
 
         markedS[s] = true;
         markedW[w] = true;
+        disToS[s] = 0;
+        disToW[w] = 0;
         quS.enqueue(s);
-        quS.enqueue(w);
+        quW.enqueue(w);
         resetS.add(s);
         resetW.add(w);
 
-        int ancestor = -1;
         boolean breakCondition = false;
 
         while (!quS.isEmpty() || !quW.isEmpty()) {
@@ -53,13 +70,14 @@ public class MyBFS {
                 for (int ad : G.adj(vertexS)) {
                     markedS[ad] = true;
                     quS.enqueue(ad);
-                    edgeToS[ad] = vertexS;
+                    // edgeToS[ad] = vertexS;
                     disToS[ad] = disToS[vertexS] + 1;
                     resetS.add(ad);
 
                     if (markedW[ad]) {
                         ancestor = ad;
                         breakCondition = true;
+                        break;
                     }
                 }
             }
@@ -72,19 +90,114 @@ public class MyBFS {
                 for (int ad : G.adj(vertexW)) {
                     markedW[ad] = true;
                     quW.enqueue(ad);
-                    edgeToW[ad] = vertexW;
+                    // edgeToW[ad] = vertexW;
                     disToW[ad] = disToW[vertexW] + 1;
                     resetW.add(ad);
 
                     if (markedS[ad]) {
                         ancestor = ad;
                         breakCondition = true;
+                        break;
+                    }
+                }
+            }
+            if (breakCondition)
+                break;
+        }
+        shortestDis = disToS[ancestor] + disToW[ancestor];
+
+        reset(resetS, resetW);
+    }
+
+    // bfs for subset
+    void bfs(Iterable<Integer> s, Iterable<Integer> w) {
+        Queue<Integer> quS = new Queue<Integer>();
+        Queue<Integer> quW = new Queue<Integer>();
+
+        ArrayList<Integer> resetS = new ArrayList<Integer>();
+        ArrayList<Integer> resetW = new ArrayList<Integer>();
+
+        for (int i : s) {
+            quS.enqueue(i);
+            markedS[i] = true;
+            disToS[i] = 0;
+            resetS.add(i);
+        }
+        for (int i : w) {
+            quW.enqueue(i);
+            markedW[i] = true;
+            disToW[i] = 0;
+            resetW.add(i);
+        }
+
+        boolean breakCondition = false;
+
+        while (!quS.isEmpty() || !quW.isEmpty()) {
+
+            // S vertex search
+            if (!quS.isEmpty()) {
+                int vertexS = quS.dequeue();
+                for (int ad : G.adj(vertexS)) {
+                    markedS[ad] = true;
+                    quS.enqueue(ad);
+                    // edgeToS[ad] = vertexS;
+                    disToS[ad] = disToS[vertexS] + 1;
+                    resetS.add(ad);
+
+                    if (markedW[ad]) {
+                        ancestor = ad;
+                        breakCondition = true;
+                        break;
                     }
                 }
             }
             if (breakCondition)
                 break;
 
+            // W vertex search
+            if (!quW.isEmpty()) {
+                int vertexW = quW.dequeue();
+                for (int ad : G.adj(vertexW)) {
+                    markedW[ad] = true;
+                    quW.enqueue(ad);
+                    // edgeToW[ad] = vertexW;
+                    disToW[ad] = disToW[vertexW] + 1;
+                    resetW.add(ad);
+
+                    if (markedS[ad]) {
+                        ancestor = ad;
+                        breakCondition = true;
+                        break;
+                    }
+                }
+            }
+            if (breakCondition)
+                break;
+        }
+        shortestDis = disToS[ancestor] + disToW[ancestor];
+
+        reset(resetS, resetW);
+    }
+
+    // return shortest distance method
+    int getShortestDis() {
+        return shortestDis;
+    }
+
+    // return common ancestor method
+    int getAncestor() {
+        return ancestor;
+    }
+
+    // resetting the arrays
+    void reset(ArrayList<Integer> resetS, ArrayList<Integer> resetw) {
+        for (int i : resetS) {
+            markedS[i] = false;
+            disToS[i] = Integer.MAX_VALUE;
+        }
+        for (int i : resetw) {
+            markedW[i] = false;
+            disToW[i] = Integer.MAX_VALUE;
         }
     }
 
--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -5,34 +5,61 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
 
     private final Digraph digraph;
+    private final MyBFS myBFS;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
         digraph = new Digraph(G);
+        myBFS = new MyBFS(G);
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-
+        myBFS.bfs(v, w);
+        return myBFS.getShortestDis();
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
+        myBFS.bfs(v, w);
+        return myBFS.getAncestor();
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        myBFS.bfs(v, w);
+        return myBFS.getShortestDis();
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        myBFS.bfs(v, w);
+        return myBFS.getAncestor();
     }
 
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        // while (!StdIn.isEmpty()) {
+        Stack<Integer> v = new Stack<Integer>();
+        v.push(13);
+        v.push(23);
+        v.push(24);
+        Stack<Integer> w = new Stack<Integer>();
+        w.push(6);
+        w.push(16);
+        w.push(17);
+        int length = sap.length(v, w);
+        int ancestor = sap.ancestor(v, w);
+        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        // }
     }
 }
