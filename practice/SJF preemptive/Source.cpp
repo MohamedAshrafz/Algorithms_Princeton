69b71adec07ff71e49b7cd8c295f3bb17eec41c9
Mohamed Ashraf <mohamed.ashraf.z879@gmail.com>
2022-04-14 15:29:38 +0200

    update -suppose to be working-

--- practice/SJF preemptive/Source.cpp
+++ practice/SJF preemptive/Source.cpp
@@ -44,66 +44,96 @@ public:
 	TimeSlot(int id, double start, double end)
 		: id{ id }, start{ start }, end{ end } {}
 
+	double getId() const {
+		return this->id;
+	}
+
+	double getStart() const {
+		return this->start;
+	}
+
 	double getEnd() const {
 		return this->end;
 	}
 
 };
 
-struct comapreBurst {
-	bool operator() (process const& a, process const& b) const {
-		return a.getBurst() < b.getBurst();
-	}
-};
+ostream& operator<<(ostream& os, TimeSlot& ts) {
+	os << ts.getId() << "\t" << ts.getStart() << "\t" << ts.getEnd();
+	return os;
+}
 
-struct comapreArrival {
+struct compareBurst {
+	bool operator() (process const& a, process const& b) const {
+		return a.getBurst() > b.getBurst();
+	}
+} myobj1;
+
+struct compareArrival {
 	bool operator() (process const& a, process const& b) const {
 		return a.getArrival() < b.getArrival();
 	}
-} myobj;
+} myobj2;
 
 int main() {
 
 	vector<process> processes;
-	sort(processes.begin(), processes.end(), myobj);
-	priority_queue<process, vector<process>, comapreBurst> pqProcesses;
+
+	processes.push_back(process(1, 0, 1));
+	processes.push_back(process(2, 2, 2));
+	processes.push_back(process(3, 3, 3));
+	processes.push_back(process(4, 5, 3));
+
+
+	sort(processes.begin(), processes.end(), myobj2);
+	priority_queue<process, vector<process>, compareBurst> pqProcesses;
 	vector<TimeSlot> vecTS;
 
 	size_t i = 0;
 	while (i < processes.size() || !pqProcesses.empty()) {
 
 		double currentArrival;
-		if (vecTS.size() == 0) {
+		if (vecTS.size() == 0)
 			currentArrival = processes.at(i).getArrival();
-			pqProcesses.push(processes.at(i));
-		}
-		else {
+		else
 			currentArrival = vecTS.at(vecTS.size() - 1).getEnd();
-		}
-		
 
-		for (i = i + 1; i < processes.size(); i++) {
-			if (currentArrival <= processes.at(i).getArrival())
+
+		for (; i < processes.size(); i++) {
+			if (currentArrival >= processes.at(i).getArrival())
 				pqProcesses.push(processes.at(i));
 			else
 				break;
 		}
 
-		double currentBurst = currentArrival - processes.at(i).getArrival();
+		double currentBurst;
+		if (i < processes.size())
+			currentBurst = processes.at(i).getArrival() - currentArrival;
+		else
+			currentBurst = DBL_MAX;
 
-		process p = pqProcesses.top();
-		pqProcesses.pop();
-		if (p.getBurst() - currentBurst > 0) {
-			TimeSlot ts1(p.getID(), currentArrival, currentArrival + currentBurst);
-			p.setBurst(p.getBurst() - currentBurst);
-			vecTS.push_back(ts1);
+		if (!pqProcesses.empty()) {
+			process p = pqProcesses.top();
+			pqProcesses.pop();
+			if (p.getBurst() - currentBurst > 0) {
+				TimeSlot ts1(p.getID(), currentArrival, currentArrival + currentBurst);
+				p.setBurst(p.getBurst() - currentBurst);
+				pqProcesses.push(p);
+				vecTS.push_back(ts1);
+			}
+			else {
+				TimeSlot ts1(p.getID(), currentArrival, currentArrival + p.getBurst());
+				vecTS.push_back(ts1);
+			}
 		}
 		else {
-			TimeSlot ts1(p.getID(), currentArrival, currentArrival + p.getBurst());
-			p.setBurst(0.0);
-			vecTS.push_back(ts1);
+			vecTS.push_back(TimeSlot(-1, currentArrival, currentArrival + currentBurst));
 		}
 	}
 
+	for(TimeSlot ts : vecTS)
+	{
+		cout << ts << endl;
+	}
 	return 0;
 }
